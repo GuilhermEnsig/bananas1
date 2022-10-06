38860dfc07009ae1efa1269697b119841e2e8974
Guitien <84877033+GuilhermEnsig@users.noreply.github.com>
2022-10-06 04:52:21 -0400

    Add files via upload

---
+++ bananas1/src/test/java/br/edu/uerr/loja/Bananas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uerr.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bananas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
